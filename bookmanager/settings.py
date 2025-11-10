51518bb5235135bcc1cd25135c9a987dc6223cd7
Nick Savaliya <nikunjsavaliya06@gmail.com>
2025-11-10 18:50:32 +0530

    databse changes

--- bookmanager/settings.py
+++ bookmanager/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key")
+DEBUG = os.getenv("DEBUG", "True").lower() == "true"
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$q6-5#jl&n=@9600@mc+)nwd_-w=0fjk7lq9wf@3^ng*_1p$64'
+# SECRET_KEY = 'django-insecure-$q6-5#jl&n=@9600@mc+)nwd_-w=0fjk7lq9wf@3^ng*_1p$64'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']  # (or ['your-app-name.onrender.com'])
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -89,18 +92,11 @@ WSGI_APPLICATION = 'bookmanager.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'book_manager',
-        'USER': 'nick1806',
-        'PASSWORD': 'Viransh@02',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL", f"postgres://nick1806:Viransh@02@localhost:5432/bookmanager"),
+        conn_max_age=600,
+        ssl_require=not DEBUG
+    )
 }
 
 
